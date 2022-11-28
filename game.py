133b962a2b1a1f2669d85cd9c51af8ac47857d8e
jaskarn-bhatti <jaskarnbhatti204@gmail.com>
2022-11-27 20:19:34 -0800

    add make_character function which returns user stats

--- game.py
+++ game.py
@@ -4,21 +4,38 @@ Student number
 """
 
 
+# from art import *
+
 def make_board(rows, colums):
     a = {}
     for length in range(colums):
         for width in range(rows):
             a[length, width] = "Empty room"
+    return a
 
-    print(a)
 
+def make_character(user_name):
+    user_stats = {"Name": user_name, "X-coordinate": 0, "Y-coordinate": 0, "Current HP": 10, "Max Hp": 10,
+                  "Strength": 3}
+    return user_stats
 
 
 def game():  # called from main
+    print("Escape The Theme Park")
+    # tprint("test")
+    print("Hello adventure")
+    print("Enter your name: ")
+    user_name = input()
+
+    # makes board dictionary
     rows = 5
     columns = 5
     board = make_board(rows, columns)
-    # character = make_character("Player name")
+
+    # Make character profile
+    character = make_character(user_name)
+    print(character)
+
     # achieved_goal = False
     # while not achieved_goal:
     # // Tell the user where they are
