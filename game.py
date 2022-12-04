137b3bbe9da13965a68ebe0f90cac45e09521ab3
jaskarn-bhatti <jaskarnbhatti204@gmail.com>
2022-12-03 21:28:32 -0800

    add challange

--- game.py
+++ game.py
@@ -4,6 +4,9 @@ Student number: A01328100
 """
 
 from art import the_big_top
+from art import clown
+from art import monkey
+import math
 
 
 def make_board(rows, columns):
@@ -20,7 +23,8 @@ def make_board(rows, columns):
 
 
 def make_character(user_name):
-    user_stats = {"Name": user_name, "X-coordinate": 0, "Y-coordinate": 0, "Current HP": 10, "Attack Damage": 5}
+    user_stats = {"Name": user_name, "X-coordinate": 0, "Y-coordinate": 0, "Current HP": 10, "Attack Damage": 5,
+                  "level": 1}
     return user_stats
 
 
@@ -46,7 +50,7 @@ def describe_current_location(board, character):
 
 
 def get_user_choice():
-    print("\nMove")
+    print("\nMovement Key")
     print("West: w  North: n  East: e  South: s")
     movement_number = str(input("Enter direction: "))
     movement_number = movement_number.lower()
@@ -56,6 +60,7 @@ def get_user_choice():
         print("Enter movement direction again")
         get_user_choice()
 
+
 def validate_move(character, direction):
     if direction == "n":
         if character["X-coordinate"] - 1 < 0:
@@ -64,12 +69,13 @@ def validate_move(character, direction):
         if character["X-coordinate"] + 1 > 4:
             return False
     elif direction == "w":
-       if character["Y-coordinate"] - 1 < 0:
-           return False
+        if character["Y-coordinate"] - 1 < 0:
+            return False
     elif direction == "e":
         if character["Y-coordinate"] + 1 > 4:
             return False
     return True
+
 
 def move_character(character, direction):
     if direction == "n":
@@ -81,15 +87,68 @@ def move_character(character, direction):
     else:
         character["Y-coordinate"] = character["Y-coordinate"] + 1
 
+
 def check_for_quest(character, board):
     if board[character["X-coordinate"], character["Y-coordinate"]] == "Quest":
         return True
     else:
         return False
 
+
 def execute_challenge_protocol(character):
-    print("Battle")
-    print("Fight the monkey to ")
+    print("\nBattle")
+    print("Fight the clown to death")
+    clown()
+    clown_stats = {"Current HP": 10, "Attack Damage": 2}
+
+    clown_dead = False
+    while not clown_dead:
+        if clown_stats["Current HP"] <= 0:
+            print("\nClown has been killed.")
+            character["level"] += 1
+            print("You have leveled up to level", character["level"])
+            character["Current HP"] += 5
+            character["Attack Damage"] += 1
+            print("Player stats\nHP:", character["Current HP"], "\nAttack Damage:", character["Attack Damage"], "\n")
+            clown_dead = True
+        else:
+            print("\nMoves\npunch: 1 kick: 2 run: 3 ")
+            move = int(input("Enter move: "))
+
+            if move == 1:
+                clown_stats["Current HP"] = clown_stats["Current HP"] - character["Attack Damage"]
+                character["Current HP"] = character["Current HP"] - clown_stats["Attack Damage"]
+                print("\nThe clown did 2 damage.")
+                print("Your HP:", character["Current HP"])
+                print("You did", character["Attack Damage"] ,"damage")
+                print("Clown HP:", clown_stats["Current HP"])
+            elif move == 2:
+                clown_stats["Current HP"] = clown_stats["Current HP"] - math.floor(character["Attack Damage"] * 0.8)
+                character["Current HP"] = character["Current HP"] - clown_stats["Attack Damage"]
+                print("\nThe clown did 2 damage.")
+                print("Your HP:", character["Current HP"])
+                print("You did", math.floor(character["Attack Damage"] * 0.8), "damage")
+                print("Clown HP:", clown_stats["Current HP"])
+            elif move == 3:
+                print("\nYou can not run from a clown.")
+                print("You got clowned for trying to run -2 HP.")
+                character["Current HP"] = character["Current HP"] - clown_stats["Attack Damage"]
+                print("Your HP:", character["Current HP"])
+                print("Clown HP:", clown_stats["Current HP"])
+            else:
+                print("\nWrong number!!!")
+                character["Current HP"] = character["Current HP"] - clown_stats["Attack Damage"]
+                print("You lost -2 HP for wrong number.")
+                print("Your HP:", character["Current HP"])
+                print("Clown HP:", clown_stats["Current HP"])
+
+def final_boss(character):
+    print("\nBattle")
+    print("Fight the clown to death")
+    mokey()
+    clown_stats = {"Current HP": 10, "Attack Damage": 2}
+
+
 
 def game():  # called from main
     print("Gravestone Circus")
@@ -97,7 +156,7 @@ def game():  # called from main
     print("April 13, 1990 you go out to watch magical monkey preform.")
     print("You arrive at the Circus to find yourself the only one in attendance.")
     print("As the show begins you see a big flash.")
-    print("Your eyes open to realize that the exit is guarded by a giant monkey\n")
+    print("Your eyes open to realize that the exit is guarded by a giant monkey.\n")
     print("Escape the Gravestone Circus !!!")
     print("Hello adventure")
     user_name = input("Enter your name: ")
@@ -114,33 +173,27 @@ def game():  # called from main
     rows = 5
     columns = 5
     board = make_board(rows, columns)
-    print(board)
 
     # Make character profile
     character = make_character(user_name)
-    print(character)
 
     achieved_goal = False
     while not achieved_goal:
         # Tell the user where they are
         describe_current_location(board, character)
 
-        # 5
         direction = get_user_choice()
-        # remove board
         valid_move = validate_move(character, direction)
-        print(valid_move)
 
         if valid_move:
             move_character(character, direction)
             there_is_a_challenge = check_for_quest(character, board)
             if there_is_a_challenge:
                 execute_challenge_protocol(character)
-    # if character_has_leveled():
-    # execute_glow_up_protocol()
+            elif board[character["X-coordinate"], character["Y-coordinate"]] == "Boss":
+                final_boss(character)
     # achieved_goal = check_if_goal_attained(board, character)
     # else:
-    # # Tell the user they can’t go in that direction
     # # Print end of game stuff like congratulations or sorry you died
     #
 
--- test.py
+++ test.py
@@ -1,21 +1,15 @@
-def describe_current_location(board, character):
-    character_pos = (character.get("X-coordinate"), character.get("Y-coordinate"))
-    board_key = list(board.keys())
-    current_board = []
-    for size in range(len(board_key)):
-        if character_pos == board_key[size]:
-            current_board.append('0')
-        elif board.get(board_key[size]) == "quest":
-            current_board.append('Q')
-        else:
-            current_board.append('X')
+clown_stats = {"Current HP": 10, "Attack Damage": 2}
+user = {"Current HP": 10, "Attack Damage": 5}
 
-    i = 0
-    while i < 5:
-        print(current_board[0:5])
-        del current_board[0:5]
-        i += 1
 
-character = {'Name': 'jsas', 'X-coordinate': 0, 'Y-coordinate': 0, 'Current HP': 10, 'Attack Damage': 5}
-board = {(0, 0): 'Empty room', (0, 1): 'Empty room', (0, 2): 'Empty room', (0, 3): 'Empty room', (0, 4): 'Empty room', (1, 0): 'Empty room', (1, 1): 'quest', (1, 2): 'Empty room', (1, 3): 'Empty room', (1, 4): 'Empty room', (2, 0): 'Empty room', (2, 1): 'Empty room', (2, 2): 'Empty room', (2, 3): 'Empty room', (2, 4): 'Empty room', (3, 0): 'Empty room', (3, 1): 'Empty room', (3, 2): 'Empty room', (3, 3): 'Empty room', (3, 4): 'Empty room', (4, 0): 'Empty room', (4, 1): 'Empty room', (4, 2): 'quest', (4, 3): 'Empty room', (4, 4): 'Empty room'}
-describe_current_location(board, character)
+clown_dead = False
+while not clown_dead:
+    print(clown_stats["Current HP"])
+    print("Moves punch: 1 kick: 2 run: 3 ")
+    # move = int(input("Enter move: "))
+    move = 1
+    if clown_stats["Current HP"] <= 0:
+        print("Clown had been killed")
+        clown_dead = True
+    elif move == 1:
+        clown_stats["Current HP"] = clown_stats["Current HP"] - user["Attack Damage"]
