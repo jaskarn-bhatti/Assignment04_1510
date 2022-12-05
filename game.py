a9a34b3fc2a28b5b745d98ee67f7203e230475be
jaskarn-bhatti <jaskarnbhatti204@gmail.com>
2022-12-04 22:53:30 -0800

    docstrings / doctest

--- game.py
+++ game.py
@@ -13,25 +13,83 @@ import itertools
 
 
 def make_board(rows, columns):
-    a = {}
+    """
+    This make_board takes in rows and columns parameter to determine how big the board should be.
+
+    It uses two for loops to populate every position with a coordinate value and a room type.
+
+    :param rows: positive int value
+    :param columns: positive int value
+    :precondition: must be a positive int values greater than 0
+    :postcondition: populates each square on board with coordinate and room type
+    :return: the board dictionary which contains room coordinate and room type
+
+    >>> make_board(5,7)
+    {(0, 0): 'Empty room', (0, 1): 'Empty room', (0, 2): 'Empty room', (0, 3): 'Empty room', (0, 4): 'Boss', (1, 0): 'Empty room', (1, 1): 'Quest', (1, 2): 'Empty room', (1, 3): 'Empty room', (1, 4): 'Empty room', (2, 0): 'Empty room', (2, 1): 'Empty room', (2, 2): 'Empty room', (2, 3): 'Empty room', (2, 4): 'Empty room', (3, 0): 'Empty room', (3, 1): 'Empty room', (3, 2): 'Empty room', (3, 3): 'Empty room', (3, 4): 'Empty room', (4, 0): 'Empty room', (4, 1): 'Empty room', (4, 2): 'Quest', (4, 3): 'Empty room', (4, 4): 'Empty room', (5, 0): 'Empty room', (5, 1): 'Empty room', (5, 2): 'Empty room', (5, 3): 'Empty room', (5, 4): 'Empty room', (6, 0): 'Empty room', (6, 1): 'Empty room', (6, 2): 'Empty room', (6, 3): 'Empty room', (6, 4): 'Empty room'}
+
+    >>> make_board(1,1)
+    {(0, 0): 'Empty room'}
+
+    >>> make_board(0,0)
+    {}
+
+    """
+    board = {}
     for length in range(columns):
         for width in range(rows):
             if length == 1 and width == 1 or length == 4 and width == 2:
-                a[length, width] = "Quest"
+                board[length, width] = "Quest"
             elif length == 0 and width == 4:
-                a[length, width] = "Boss"
+                board[length, width] = "Boss"
             else:
-                a[length, width] = "Empty room"
-    return a
+                board[length, width] = "Empty room"
+    return board
 
 
 def make_character(user_name):
+    """
+    Make_character accepts the user_name from an input and then creates a dictionary with user info which contains
+    username.
+
+    :param user_name: string
+    :precondition: user enters a string
+    :postcondition: creates user dictionary and adds a user_name from the input
+    :return: dictionary with user_stats
+
+    >>> make_character("a")
+    {'Name': 'a', 'X-coordinate': 0, 'Y-coordinate': 0, 'Current HP': 10, 'Attack Damage': 5, 'level': 0}
+
+    >>> make_character("chris")
+    {'Name': 'chris', 'X-coordinate': 0, 'Y-coordinate': 0, 'Current HP': 10, 'Attack Damage': 5, 'level': 0}
+
+    >>> make_character("Celestial Dragon")
+    {'Name': 'Celestial Dragon', 'X-coordinate': 0, 'Y-coordinate': 0, 'Current HP': 10, 'Attack Damage': 5, 'level': 0}
+
+    >>> make_character("123")
+    {'Name': '123', 'X-coordinate': 0, 'Y-coordinate': 0, 'Current HP': 10, 'Attack Damage': 5, 'level': 0}
+
+    >>> make_character("$")
+    {'Name': '$', 'X-coordinate': 0, 'Y-coordinate': 0, 'Current HP': 10, 'Attack Damage': 5, 'level': 0}
+
+    """
     user_stats = {"Name": user_name, "X-coordinate": 0, "Y-coordinate": 0, "Current HP": 10, "Attack Damage": 5,
                   "level": 0}
     return user_stats
 
 
 def describe_current_location(board, character):
+    """
+    Takes board and character parameter and crates a dictionary which contains characters which represent the board.
+
+    The dictionary is taken into a while loop and prints out each row.
+
+    :param board: a dictionary with coordinates and room type
+    :param character: a dictionary with x and y coordinates
+    :precondition: board dictionary containing room types and character dictionary with x y coordinates
+    :postcondition: prints each row of board with values
+
+
+    """
     character_pos = (character.get("X-coordinate"), character.get("Y-coordinate"))
     board_key = list(board.keys())
     current_board = []
@@ -45,14 +103,22 @@ def describe_current_location(board, character):
         else:
             current_board.append('X')
 
-    i = 0
-    while i < 5:
+    int_iterator = 0
+    while int_iterator < 5:
         print(current_board[0:5])
         del current_board[0:5]
-        i += 1
+        int_iterator += 1
 
 
 def get_user_choice():
+    """
+    Ask user to enter a direction to move and can type quit to quit the game.
+
+    :precondition: user must enter w,n,s,e to move or quit to quit
+    :postcondition: determines what direction the user wants to move or quit
+    :return: direction the user wants to move or quit
+
+    """
     print("\nMovement Key")
     print("West: w  North: n  East: e  South: s")
     movement_number = str(input("Enter direction: "))
@@ -67,6 +133,46 @@ def get_user_choice():
 
 
 def validate_move(character, direction):
+    """
+    Checks if the user is able to move in that direction.
+
+    :param character: a dictionary with x and y coordinates
+    :param direction: a char either n,s,w,e
+    :precondition: the dictionaries must contain correct values
+    :postcondition: if move is feasible return true
+    :return: true or false depending on if move is feasible
+
+    >>> character = {'Name': '$', 'X-coordinate': 0, 'Y-coordinate': 0, 'Current HP': 10, 'Attack Damage': 5, 'level': 0}
+    >>> direction = "e"
+    >>> validate_move(character, direction)
+    True
+
+    >>> character = {'Name': '$', 'X-coordinate': 0, 'Y-coordinate': 0, 'Current HP': 10, 'Attack Damage': 5, 'level': 0}
+    >>> direction = "w"
+    >>> validate_move(character, direction)
+    False
+
+    >>> character = {'Name': '$', 'X-coordinate': 0, 'Y-coordinate': 0, 'Current HP': 10, 'Attack Damage': 5, 'level': 0}
+    >>> direction = "n"
+    >>> validate_move(character, direction)
+    False
+
+    >>> character = {'Name': '$', 'X-coordinate': 0, 'Y-coordinate': 0, 'Current HP': 10, 'Attack Damage': 5, 'level': 0}
+    >>> direction = "s"
+    >>> validate_move(character, direction)
+    True
+
+    >>> character = {'Name': '$', 'X-coordinate': 1, 'Y-coordinate': 0, 'Current HP': 10, 'Attack Damage': 5, 'level': 0}
+    >>> direction = "n"
+    >>> validate_move(character, direction)
+    True
+
+     >>> character = {'Name': '$', 'X-coordinate': 0, 'Y-coordinate': 1, 'Current HP': 10, 'Attack Damage': 5, 'level': 0}
+    >>> direction = "w"
+    >>> validate_move(character, direction)
+    True
+
+    """
     if direction == "n":
         if character["X-coordinate"] - 1 < 0:
             return False
@@ -83,6 +189,13 @@ def validate_move(character, direction):
 
 
 def move_character(character, direction):
+    """
+    Moves the player x and y depending on the direction.
+
+    :param character: a dictionary with x and y coordinates
+    :param direction: a char either n,s,w,e
+
+    """
     if direction == "n":
         character["X-coordinate"] = character["X-coordinate"] - 1
     elif direction == "s":
@@ -94,6 +207,15 @@ def move_character(character, direction):
 
 
 def check_for_quest(character, board):
+    """
+    Checks if user is on the quest coordinates.
+
+    :param character: a dictionary with x and y coordinates
+    :param board: a dictionary with coordinates and room type
+    :return: True or False if on quest
+
+
+    """
     if board[character["X-coordinate"], character["Y-coordinate"]] == "Quest":
         return True
     else:
@@ -101,6 +223,12 @@ def check_for_quest(character, board):
 
 
 def execute_challenge_protocol(character):
+    """
+    Calls the clown to fight
+
+    :param character: a dictionary with x and y coordinates
+    :return: true or false if you beat the clowm
+    """
     print("\nBattle")
     print("Fight the clown to death")
     clown()
@@ -151,6 +279,12 @@ def execute_challenge_protocol(character):
 
 
 def final_boss(character):
+    """
+    Calls the finial boss to fight.
+
+    :param character: a dictionary with x and y coordinates
+    :return:
+    """
     print("\nBattle")
     print("Fight the Monkey to death")
     monkey()
@@ -181,7 +315,7 @@ def final_boss(character):
                 character["Current HP"] = character["Current HP"] - monkey_stats["Attack Damage"]
                 print("\nThe Monkey did 5 damage.")
                 print("Your HP: %s" % character["Current HP"])
-                print("You did %s damage" % math.floor(character["Attack Damage"] * 0.8) )
+                print("You did %s damage" % math.floor(character["Attack Damage"] * 0.8))
                 print("Monkey HP: %s" % monkey_stats["Current HP"])
             elif move == 3:
                 print("\nYou can not run from the Boss!")
@@ -198,12 +332,22 @@ def final_boss(character):
 
 
 def dead():
+    """
+    Play the finial scene when the user dies.
+    """
     print("\nyou fought a good fight.")
     death()
     print("GAME OVER")
+    six_six = list(itertools.repeat(666, 6))
+    for numbers in list(range(len(six_six))):
+        print(six_six[numbers])
 
 
 def game():  # called from main
+    """
+     Runs the game.
+
+    """
     print("Gravestone Circus")
     the_big_top()
     print("April 13, 1990 you go out to watch magical monkey preform.")
@@ -251,9 +395,6 @@ def game():  # called from main
                     achieved_goal = True
             elif board[character["X-coordinate"], character["Y-coordinate"]] == "Boss":
                 achieved_goal = final_boss(character)
-    # achieved_goal = check_if_goal_attained(board, character)
-    # else:
-    # Print end of game stuff like congratulations or sorry you died
 
 
 def main():
